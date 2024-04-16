5888180ac390f4ef4ea32c8cb79940993d4d2465
kartik vaghani <kartikvaghani066@gmail.com>
2024-04-16 12:43:54 +0530

    complete all update, delete functionality

--- api/index.js
+++ api/index.js
@@ -4,7 +4,8 @@ import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
-dotenv.config()
+import path from 'path';
+dotenv.config();
 
 mongoose
   .connect(process.env.MONGO)
@@ -13,26 +14,35 @@ mongoose
   })
   .catch((err) => {
     console.log(err);
-});
+  });
+
+const __dirname = path.resolve();
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
+
 app.use(express.json());
+
 app.use(cookieParser());
 
 app.listen(3000, () => {
-    console.log('Server listning on port 3000');
+  console.log('Server listening on port 3000');
 });
 
-app.use("/api/user", userRoutes);
-app.use("/api/auth", authRoutes);
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
 
 app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || 'Internal Server Error';
-    return res.status(statusCode).json({
-      success: false,
-      message,
-      statusCode,
-    });
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode,
   });
-  
+});
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -2,8 +2,8 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   currentUser: null,
-  error: null,
   loading: false,
+  error: false,
 };
 
 const userSlice = createSlice({
@@ -16,15 +16,55 @@ const userSlice = createSlice({
     signInSuccess: (state, action) => {
       state.currentUser = action.payload;
       state.loading = false;
-      state.error = null;
+      state.error = false;
     },
     signInFailure: (state, action) => {
-      state.error = action.payload;
       state.loading = false;
+      state.error = action.payload;
     },
-     },
+    updateUserStart: (state) => {
+      state.loading = true;
+    },
+    updateUserSuccess: (state, action) => {
+      state.currentUser = action.payload;
+      state.loading = false;
+      state.error = false;
+    },
+    updateUserFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    deleteUserStart: (state) => {
+      state.loading = true;
+    },
+    deleteUserSuccess: (state) => {
+      state.currentUser = null;
+      state.loading = false;
+      state.error = false;
+    },
+    deleteUserFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    signOut: (state) => {
+      state.currentUser = null;
+      state.loading = false;
+      state.error = false;
+    },
+  },
 });
 
-export const { signInStart, signInSuccess,signInFailure } = userSlice.actions;
+export const {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+  updateUserFailure,
+  updateUserStart,
+  updateUserSuccess,
+  deleteUserFailure,
+  deleteUserStart,
+  deleteUserSuccess,
+  signOut,
+} = userSlice.actions;
 
 export default userSlice.reducer;
